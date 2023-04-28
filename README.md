6b141d542f45cbc2e749c95273d74e3fb9006d38
Antoine Turmel <antoine.social@turmel.info>
2023-04-28 15:41:07 +0200

    import v1 from private repo

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# kontoj
-A tool to help create accounts on services, based on a source of services.
+# kontojkreado
+
+![Logo](images/../src/images/logo-corners-h100.png)
+
+[![Donate PayPal.Me](https://img.shields.io/badge/Donate-PayPal.Me-8bd7bc?style=for-the-badge&logo=paypal)](https://www.paypal.me/AntoineTURMEL)
+
+A web tool to help create accounts on web services, based on a JSON file.
+
+## How to use Kontoj v1:
+### Load your custom services:
+- Copy [src/services.example.json](src/services.example.json) from project
+- Fill your services details inside this file (you can validate your file using this schema: [src/services.schema.json](src/services.schema.json), you can also edit the file using [web json-editor](https://pmk65.github.io/jedemov2/dist/demo.html))
+- In Kontoj, browse for your services.json file, then click on « Load » to load the list of services.
+
+💡 **NOTE: In future version, it will be possible to save the JSON file inside the browser.**
+
+### Generate and fill emails/passwords
+- On the top bar, you can fill the Last Name/First Name, Email of the person
+- You can either check manually each service, check service group or select user Group in top bar
+- You can use the « Fill » ⏬ button to copy the mail into each email fields of selected services
+- You can use the « Password » 🔑 button to generate a password (based on rules defined in config.json) for the selected service, or every services checked (if clicked from top bar)
+- You can use the « Generate mail » 📨 button to generate a mail body with all credentials (currently using mailto: with html-body only available in Mozilla Thunderbird)
+
+### Login & Create Account
+- Using « Login » 🚪 button you can go to the login page of the service
+- Using « Create Account » 👤 button you can go the create account page of the service
+
+💡 **NOTE: In future version, it will be possible to automatically fill username/email/password when clicking on the «Create Account » 👤 button using Kontoj Companion.**
+
+### Live Demo
+
+Load services & fill mail/password
+![Demo](src/images/demo_v1.gif)
+
+Generated mail
+![Mail body](src/images/demo_mail_v1.png)
+
+## Dev instructions:
+- Install nodejs/npm
+- npm install
+- npx serve src
+
+## Licence
+
+Kontoj is under [MIT Licence](/LICENSE)
---
+++ src/js/script.js
@@ -0,0 +1,362 @@
+$( document ).ready(function() {
+
+jQuery.fn.extend({
+  modal: function(action) {
+    return this.each(function() {
+      if (this.tagName === "DIALOG") {
+        if (action === "showModal") {
+          this.showModal();
+        } else if (action === "close") {
+          this.close();
+        } else if (action === "getBoundingClientRect") {
+          this.getBoundingClientRect();
+        }
+      }
+    });
+  }
+});
+
+
+const newLineMailTo = "%0D%0A";
+var subjectMail = "";
+var bodyMailStart = "";
+var bodyMailEnd = "";
+var passwordLength = 20;
+var passwordChars = "";
+
+// strings
+var serviceUrlStr = "";
+var emailStr = "";
+var loginStr = "";
+var googleStr = "";
+var passwordStr = "";
+
+async function loadStrings() {
+  serviceUrlStr = await document.l10n.formatValue('service-mailbody-url');
+  emailStr = await document.l10n.formatValue('service-mailbody-email');
+  loginStr = await document.l10n.formatValue('service-mailbody-login');
+  googleStr = await document.l10n.formatValue('service-login-google-account');
+  passwordStr = await document.l10n.formatValue('service-mailbody-password');
+}
+
+loadStrings();
+
+$(".dialogButton").on("click", function(e){
+  $("#" + $(this).data("dialog-id")).modal("showModal");
+});
+
+$("dialog").on("click", function(e){
+  var rect = $(e.target).modal("getBoundingClientRect");
+  var isInDialog=(rect.top <= event.clientY && event.clientY <= rect.top + rect.height
+    && rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
+  if (!isInDialog) {
+    $(e.target).modal("close");
+  }
+});
+
+function cleanString(string, prefix) {
+  string = string.replaceAll(" ", "_").replaceAll("é","e").replaceAll(".", "").toLowerCase();
+  if(prefix) {
+    string = prefix + string;
+  }
+  return string;
+}
+
+if(localStorage.getItem('config')) {
+  //TODO: load the saved json file content
+  //$("#valueConfig").val(localStorage.getItem('config'));
+  //loadConfig(localStorage.getItem('config'));
+}
+
+$("#saveConfig").on("click", function() {
+  localStorage.setItem('config', $("#valueConfig").val());
+});
+
+$("#loadConfig").on("click", function() {
+  //valueConfig = localStorage.getItem('config');
+  //$("#valueConfig").val(valueConfig);
+  loadConfig();
+});
+
+$("#clearConfig").on("click", function() {
+  localStorage.removeItem('config');
+});
+
+$.getJSON("manifest.json", function( manifest ) {
+  $("#version").text($("#version").text() + " " + manifest.version);
+  if(manifest.build != "") {
+    $("#version").text($("#version").text() + " - build " + manifest.build);
+  }
+  $("#projectUrl").attr("href",manifest.repository.url);
+  $("#projectUrl").text(manifest.repository.url);
+  $("#licenceUrl").attr("href",manifest.repository.url + '/blob/main/LICENSE');
+  $("#docUrl").attr("href",manifest.documentation);
+  $("#docUrl").text(manifest.documentation);
+});
+
+function loadConfig() {
+  $('#myAccordion').empty();
+  $('#userGroup').empty();
+  $('#userGroup').append($('<option>', {
+    value: "",
+    text: "---",
+    selected:"selected"
+  }));
+
+  var fileInput = $('#jsonFile');
+  var file = fileInput[0].files[0];
+  var reader = new FileReader();
+
+  var accordionHtml = '';
+
+  reader.onload = function() {
+    var data = JSON.parse(reader.result);
+
+    var servicesGroups = [];
+    var userGroups = [];
+
+    $.each(data.records, function(key, val) {
+      servicesGroups.indexOf(val.fields["Service Group"]) === -1 ? servicesGroups.push(val.fields["Service Group"]) : console.log("This item already exists");
+      $.each(val.fields["User Group"], function(key, val) {
+        userGroups.indexOf(val) === -1 ? userGroups.push(val) : console.log("This item already exists");
+      });
+    });
+
+    $.each(userGroups, function(key, val) {
+      $('#userGroup').append($('<option>', {
+        value: val,
+        text: val
+      }));
+    });
+
+    $.each(servicesGroups, function(key, val) {
+      accordionHtml += '<div class="accordion-item" id="AcItem_' + cleanString(val) + '">';
+      accordionHtml += '<h2 class="accordion-header"><input type="checkbox" class="larger cbg" id="cbg_'+ cleanString(val) +'" data-service-group="'+ cleanString(val) +'" />';
+      accordionHtml += '<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseAc_' + cleanString(val) + '">' + val + '</button>';
+      accordionHtml += '</h2>';
+      accordionHtml += '<div id="collapseAc_' + cleanString(val) + '" class="accordion-collapse collapse">';
+      accordionHtml += '<div class="accordion-body"></div>';
+      accordionHtml += '</div>';
+      accordionHtml += '</div>';
+    });
+
+    $('#myAccordion').append(accordionHtml);
+
+    $.each(data.records, function(key, val) {
+      accordionItemHtml =  '';
+      accordionItemHtml +=  '<div class="row align-items-center service-row" data-password-type="' + val.fields["Password Type"] + '" data-service-name="' + cleanString(val.fields["Name"], "service_") + '">';
+      var userGroups = "";
+      if(val.fields["User Group"]) {
+        userGroups = val.fields["User Group"].join(',');
+      }
+      accordionItemHtml +=  '<div class="col-1 my-auto"><input type="checkbox" class="larger cbs" id="cbs_' + cleanString(val.fields["Name"], "service_") + '" data-service-group="' + cleanString(val.fields["Service Group"]) + '" data-user-groups="' + userGroups + '"/></div>';
+      if(val.fields["Icon"] == null || val.fields["Icon"] == "") {
+        val.fields["Icon"] = "/images/default_service_icon.svg";
+      }
+      accordionItemHtml +=  '<div class="col-2"><img src="' + val.fields["Icon"] + '" class="service_logo" alt="service logo"/> <span class="serviceNameClean">' + val.fields["Name"] + '</span></div>';
+
+      if(val.fields["Password Type"] == "email-password" || val.fields["Password Type"] == "third-party") {
+        accordionItemHtml +=  '<div class="col-5">';
+        accordionItemHtml +=  '<input class="emailfield" data-l10n-id="service-email" type="email"/> ';
+        accordionItemHtml +=  '<input class="passwordfield" data-l10n-id="service-password" type="text"/> ';
+        accordionItemHtml +=  '<a data-l10n-id="service-generate-password" class="icon-button gen_pass_service">🔑</a> ';
+        accordionItemHtml +=  '<a data-l10n-id="service-unlock-password" class="icon-button unlock_pass">🔓</a> ';
+        if(val.fields["Password Notes"]) {
+          accordionItemHtml +=  '<span class="password-notes" title="' + val.fields["Password Notes"] + '">❓</span>';
+        }
+        accordionItemHtml +=  '</div>';
+      }
+      if(val.fields["Password Type"] == "login-password") {
+        accordionItemHtml +=  '<div class="col-5">';
+        accordionItemHtml +=  '<input class="loginfield" data-l10n-id="service-login" type="text"/> ';
+        accordionItemHtml +=  '<input class="passwordfield" data-l10n-id="service-password" type="text"/> ';
+        accordionItemHtml +=  '<a data-l10n-id="service-generate-password" class="icon-button gen_pass_service">🔑</a> ';
+        accordionItemHtml +=  '<a data-l10n-id="service-unlock-password" class="icon-button unlock_pass">🔓</a> ';
+        if(val.fields["Password Notes"]) {
+          accordionItemHtml +=  '<span class="password-notes" title="' + val.fields["Password Notes"] + '">❓</span>';
+        }
+        accordionItemHtml +=  '</div>';
+      }
+      if(val.fields["Password Type"] == "google-account") {
+        accordionItemHtml +=  '<div class="col-5"><i><span data-l10n-id="service-login-google-account"></span></i></div>';
+      }
+      if(val.fields["URL Login"]) {
+        accordionItemHtml +=  '<div class="col-1"><a href="' + val.fields["URL Login"] + '" target="_blank" class="url-login icon-button" data-l10n-id="service-action-login">🚪</a></div>';
+      }
+      if(val.fields["URL Create Account"]) {
+        accordionItemHtml +=  '<div class="col-1"><a href="' + val.fields["URL Create Account"] + '" target="_blank" class="url-create-account icon-button" data-l10n-id="service-action-create-account">👤</a></div>';
+      }
+      accordionItemHtml +=  '</div>';
+      $('#AcItem_' + cleanString(val.fields["Service Group"]) + ' .accordion-body').append(accordionItemHtml);
+    });
+
+    $('.accordion-item').each(function() {
+      $(this).find('.accordion-button').attr('aria-expanded', 'true');
+      $(this).find('.accordion-collapse').addClass('collapse show');
+    });
+
+    // Check/Uncheck all service from a group
+    $(".cbg").change(function() {
+      var group = $(this).data("service-group");
+      if(this.checked) {
+        $(".cbs[data-service-group=" + group + "]").prop('checked', true);
+      }
+      if(!this.checked) {
+        $(".cbs[data-service-group=" + group + "]").prop('checked', false);
+      }
+    });
+
+    // Check services based on user group
+    $("#userGroup").on("change", function(){
+      var userGroup = $(this).find(":selected").val();
+      $(".cbs").each(function() {
+        if ($(this).data("user-groups").split(',').includes(userGroup) && userGroup != "") {
+          $(this).prop('checked', true);
+        } else {
+          $(this).prop('checked', false);
+        }
+      });
+    });
+
+  subjectMail = data.subjectMail;
+  bodyMailStart = data.bodyMailStart;
+  bodyMailEnd = data.bodyMailEnd;
+  passwordLength = data.passwordLength;
+  passwordChars = data.passwordChars;
+
+  };
+  reader.readAsText(file);
+}
+
+function genPassword(length, chars) {
+  var passwordLength = length;
+  var password = "";
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+  }
+  return password;
+}
+
+$("body").on("click", '.gen_pass_service', function() {
+  var serviceName = $(this).parent().parent().data("service-name");
+  if (!$('[data-service-name="'+serviceName+'"] .passwordfield').prop("disabled")) {
+    $('[data-service-name="'+serviceName+'"] .passwordfield').val(genPassword(passwordLength, passwordChars));
+  }
+});
+
+$("body").on("click", '.url-create-account', function() {
+  var serviceName = $(this).parent().parent().data("service-name");
+  $('[data-service-name="'+serviceName+'"] .passwordfield').prop("disabled", "disabled");
+  $('[data-service-name="'+serviceName+'"] .passwordfield').css("background-color", "#1c2440");
+  $('[data-service-name="'+serviceName+'"] .unlock_pass').css("display", "inline");
+  $('[data-service-name="'+serviceName+'"] .gen_pass_service').css("display", "none");
+});
+
+$("body").on("click", '.unlock_pass', function() {
+  var serviceName = $(this).parent().parent().data("service-name");
+  $('[data-service-name="'+serviceName+'"] .passwordfield').prop("disabled", false);
+  $('[data-service-name="'+serviceName+'"] .passwordfield').css("background-color", "");
+  $('[data-service-name="'+serviceName+'"] .unlock_pass').css("display", "none");
+  $('[data-service-name="'+serviceName+'"] .gen_pass_service').css("display", "inline");
+});
+
+function checkIfAtLeastOneServiceChecked() {
+  if(!$(".service-row").find('input[type="checkbox"]').is(":checked")) {
+    async function alertEmptyServices() {
+      let msg = await document.l10n.formatValue('general-no-services-checked');
+      alert(msg);
+    }
+    alertEmptyServices();
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// Generate and fill password on services checked
+$("#genPassAll").on("click", function(){
+  $(".service-row").each(function() {
+    if($(this).find('input[type="checkbox"]').is(":checked")) {
+      var serviceName = $(this).data("service-name");
+      if (!$('[data-service-name="'+serviceName+'"] .passwordfield').prop("disabled")) {
+        $('[data-service-name="'+serviceName+'"] .passwordfield').val(genPassword(passwordLength, passwordChars));
+      }
+    }
+  })
+  checkIfAtLeastOneServiceChecked();
+});
+
+// Fill email address on services checked
+$("#fillEmail").on("click", function(){
+  $(".service-row").each(function() {
+    if($(this).find('input[type="checkbox"]').is(":checked")) {
+      var serviceName = $(this).data("service-name")
+      $('[data-service-name="'+serviceName+'"] input[type=email').val($("#userEmail").val());
+    }
+  })
+  checkIfAtLeastOneServiceChecked();
+});
+
+$('#genMail').on('click', function (event) {
+  event.preventDefault();
+
+  async function alertEmptyMail() {
+    let msg = await document.l10n.formatValue('general-empty-email');
+    alert(msg);
+    $("#userEmail").focus();
+  }
+
+  if($("#userEmail").val() == "") {
+    alertEmptyMail();
+  } else {
+    if(checkIfAtLeastOneServiceChecked()) {
+      var email = $("#userEmail").val();
+      var subject = subjectMail.replaceAll("_firstname_", $("#userFirstName").val()).replaceAll("_lastname_", $("#userLastName").val());
+      var emailBody = genBodyMail();
+      window.location = 'mailto:' + email + '?subject=' + subject + '&html-body=' +   emailBody;
+    }
+  }
+});
+
+function genBodyMail() {
+  var emailBody = '';
+  if(bodyMailStart != "") {
+    emailBody += bodyMailStart.replaceAll("_firstname_", $("#userFirstName").val()).replaceAll("_lastname_", $("#userLastName").val());
+    emailBody += "<br/><br/>";
+  }
+  $(".service-row").each(function() {
+    if($(this).find('input[type="checkbox"]').is(":checked")) {
+      var serviceName = $(this).data("service-name");
+      emailBody += "<b>"+$('[data-service-name="'+serviceName+'"] .serviceNameClean').text()+"</b>";
+      emailBody += "<br/>";
+      if($('[data-service-name="'+serviceName+'"] .url-login').prop("href") != null) {
+        emailBody += serviceUrlStr + " <a href='"+$('[data-service-name="'+serviceName+'"] .url-login').prop("href")+"'>"+$('[data-service-name="'+serviceName+'"] .url-login').prop("href")+"</a>";
+        emailBody += "<br/>";
+      }
+      if($(this).data("password-type") == "google-account") {
+        emailBody += googleStr;
+        emailBody += "<br/>";
+      } else {
+        if($(this).data("password-type") == "email-password") {
+          emailBody += emailStr + " " + $('[data-service-name="'+serviceName+'"] .emailfield').val();
+          emailBody += "<br/>";
+        }
+        if($(this).data("password-type") == "login-password") {
+          emailBody += loginStr + " " + $('[data-service-name="'+serviceName+'"] .loginfield').val();
+          emailBody += "<br/>";
+        }
+        emailBody += passwordStr + " " + $('[data-service-name="'+serviceName+'"] .passwordfield').val();
+        emailBody += "<br/>";
+      }
+      emailBody += "<br/>";
+      if(bodyMailEnd != "") {
+        emailBody += bodyMailEnd;
+      }
+    }
+  })
+  console.log(emailBody);
+  return emailBody;
+}
+
+});
